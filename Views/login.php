2292d3d8f80928db4d1584bdd5c229d027e78bc0
Brian Villamil <bryanvillamil13@gmail.com>
2026-03-24 15:50:50 -0500

    hice la pagina de login

---
+++ Views/login.php
@@ -0,0 +1,18 @@
+<h2>Ingresar al Sistema - KAWAK</h2>
+
+<form action="index.php?action=autenticar" method="POST">
+    
+    <label>Usuario:</label><br>
+    <input type="text" name="usuario" placeholder="admin" required>
+    <br><br>
+
+    <label>Contraseña:</label><br>
+    <input type="password" name="password" placeholder="1234" required>
+    <br><br>
+
+    <button type="submit">Entrar</button>
+    
+</form>
+
+<br>
+<p>Prueba con admin / 1234</p>
